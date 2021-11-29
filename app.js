764696d1f3159687cde50dc25499a035931d63ba
MatteoL-petitordi <matteo.ftt@gmail.com>
2021-11-29 12:47:26 +0100

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const pageContainer = document.querySelector("[data-scroll-container]");
 const scroller = new LocomotiveScroll({
     el: pageContainer,
     smooth: true,
-    multiplier: 0.3,
+    multiplier: 0.6,
     smartphone: {
         smooth: true
     }
@@ -27,57 +27,4 @@ ScrollTrigger.scrollerProxy(pageContainer, {
         };
     },
     pinType: pageContainer.style.transform ? "transform" : "fixed"
-});
-
-////////////////////////////////////
-////////////////////////////////////
-window.addEventListener("load", function () {
-    let allBigPhrases = gsap.utils.toArray('strong');
-    allBigPhrases.forEach((phrase) => {
-        gsap.to(phrase, {
-            scrollTrigger: {
-                trigger: phrase,
-                scroller: pageContainer,
-                scrub: true,
-                start: 'top 85%',
-            },
-            opacity: 7,
-            ease: "power1.inOut"
-        })
-    });
-
-    let allSections = gsap.utils.toArray('section');
-    allSections.forEach((section) => {
-        let className = section.className;
-
-        ScrollTrigger.create({
-            trigger: 'section.' + className,
-            scroller: pageContainer,
-            scrub: true,
-            start: 'top center',
-            toggleClass: {
-                targets: "body",
-                className: className
-            }
-        })
-    })
-
-    let allInc = gsap.utils.toArray('.inc');
-    allInc.forEach((strong) => {
-        gsap.fromTo(strong, {
-            x: 100
-        }, {
-            scrollTrigger: {
-                trigger: strong,
-                scroller: pageContainer,
-                scrub: true,
-                start: 'top 85%',
-            },
-            x: 0,
-        })
-    });
-
-    ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
-
-    ScrollTrigger.refresh();
 });
